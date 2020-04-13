ac67651a90a7fc03f6410423b881d3b49cd43264
J. Freitas <juniormfreitas@gmail.com>
2020-04-13 13:59:42 -0600

    Removing backticks

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,67 +7,68 @@ const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://jfreitas.photos`,
-    headline: `Freitas.eye`,
-    greeting: `Hi! My name is`,
-    name: `J. de Freitas`,
+    siteUrl: "https://jfreitas.photos",
+    headline: "Freitas.eye",
+    greeting: "Hi! My name is",
+    name: "J. de Freitas",
     author: this.name,
-    title: `Photographer`,
-    description: `This is my photography portfolio that shows how I see the world with my eyes.`,
+    title: "Photographer",
+    description:
+      "This is my photography portfolio that shows how I see the world with my eyes.",
     externalSites: [
       {
-        name: `Behance`,
-        url: `https://www.behance.net/freitaseye`,
+        name: "Behance",
+        url: "https://www.behance.net/freitaseye"
       },
       {
-        name: `Instagram`,
-        url: `https://instagram.com/freitas.eye`,
+        name: "Instagram",
+        url: "https://instagram.com/freitas.eye"
       },
       {
-        name: `About Me`,
-        url: `https://jfreitas.dev`,
-      },
-    ],
+        name: "About Me",
+        url: "https://jfreitas.dev"
+      }
+    ]
   },
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `gallery`,
-        path: path.join(__dirname, `src/img/gallery`),
-      },
+        name: "gallery",
+        path: path.join(__dirname, "src/img/gallery")
+      }
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
-        fonts: [`Roboto Slab:400,700`],
-        display: "swap",
-      },
+        fonts: ["Roboto Slab:400,700"],
+        display: "swap"
+      }
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `jfreitas.photos`,
-        short_name: `jfreitas.photos`,
-        start_url: `/`,
-        background_color: `#1a1a1a`,
-        theme_color: `#1a1a1a`,
-        display: `standalone`,
-        icon: `src/img/favicon/icon.png`,
-      },
+        name: "jfreitas.photos",
+        short_name: "jfreitas.photos",
+        start_url: "/",
+        background_color: "#1a1a1a",
+        theme_color: "#1a1a1a",
+        display: "standalone",
+        icon: "src/img/favicon/icon.png"
+      }
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-robots-txt`,
+    "gatsby-plugin-offline",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-robots-txt",
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "UA-75050740-2",
-      },
-    },
-  ],
+        trackingId: "UA-75050740-2"
+      }
+    }
+  ]
 };
