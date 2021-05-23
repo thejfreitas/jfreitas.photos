952683012ed606f778d2b28990249320fec77d17
J. de Freitas <juniormfreitas@gmail.com>
2021-05-23 15:21:20 -0600

    Remove hardcoded info out of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import { getFullCurrentYear } from '../utils'
 
 export default function Footer() {
+  const siteDataQuery = useStaticQuery(
+    graphql`
+    query FooterData {
+      site {
+        siteMetadata {
+          siteAddress
+          repository
+        }
+      }
+    }
+  `
+  );
+
+  const { siteAddress, repository } = siteDataQuery.site.siteMetadata;
+
   return (
     <footer className="container">
-      <p>jfreitas.photos {getFullCurrentYear()} &copy; All rights reserved</p>
+      <p>{siteAddress} {getFullCurrentYear()} &copy; All rights reserved</p>
       <p className="github-link">
         <a href="https://github.com/juniormfreitas/jfreitas.photos">
-          How I built this website.
+          {repository}
         </a>
       </p>
     </footer>
